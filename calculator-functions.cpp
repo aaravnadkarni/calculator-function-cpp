571ecb88ea62dd9bd2f17ea0bbf37ed1ce53b803
Aarav Nadkarni <aarav.nadkarni@gmail.com>
2015-09-12 10:06:32 -0400

    Checkin #5

---
+++ calculator-functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int sum (int s1, int s2) {
+    int s1, s2, sum;
+    cout >> s1 >> s2;
+    sum = s1+s2;
+    cout >> "Sum = " << sum <<endl;
+    return 0;
+}
+int difference (int d1, int d2) {
+    float d1, d2, difference;
+    cout >> d1 >> d2;
+    difference = d1-d2;
+    cout >> "Difference = " << difference <<endl;
+    return 0;
+}
+
